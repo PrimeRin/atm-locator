2a6f7d16a03009b11a41d7c913efddf1c87c9af9
PrimeRin <primedruk312@gmail.com>
2024-05-01 11:30:28 +0600

    footer completed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,95 @@ import React, { useState } from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return (
+ return (
     <div className="footer-con">
-      FOOTER
+      <div className="links">
+        <h6>Links</h6>
+        <ul>
+          <li><a href="https://www.rma.org.bt/fid/">Department of Financial Intelligence</a></li>
+          <li><a href="https://www.rma.org.bt/paymentsystem.jsp">Payment Systems</a></li>
+          <li><a href="https://www.rma.org.bt/onlinesurvey.jsp" target="_blank">Publication User Survey</a></li>
+          <li><a href="http://www.saarcfinance.org/" target="_blank">SAARC Finance</a></li>
+          <li><a href="https://www.rma.org.bt/SAIR.jsp">SAI Regulators</a></li>
+          <li><a href="http://www.rma.org.bt:8080/fil" target="_blank">Financial Inclusion &amp; Literacy</a></li>
+          <li><a href="https://www.rma.org.bt/forms/unsanctionlist.pdf" target="_blank">Consolidated UN Security Council Sanctions List</a></li>
+          <li><a href="http://www.befit.bt/" target="_blank">BEFIT</a></li>
+          <li><a href="https://www.rma.org.bt/forms/engversion.pdf">Know Your Money</a></li>
+        </ul>
+      </div>
+
+      <div className="links">
+        <h6>Financial Institutions</h6>
+        <ul>
+          <li><a href="https://www.bob.bt/" target="_blank">Bank of Bhutan</a></li>
+          <li><a href="https://www.bnb.bt/" target="_blank">Bhutan National Bank Ltd</a></li>
+          <li><a href="http://drukpnbbank.bt/" target="_blank">Druk PNB Bank Ltd</a></li>
+          <li><a href="http://bdb.bt/" target="_blank">Bhutan Development Bank Ltd</a></li>
+          <li><a href="http://www.tbank.bt/" target="_blank">T Bank</a></li>
+          <li><a href="https://www.digitalkidu.bt/" target="_blank">DK Limited</a></li>
+        </ul>
+      </div>
+
+      <div className="links">
+        <h6>Non-Bank Institutions</h6>
+        <ul>
+          <li><a href="https://www.ricb.bt/index.php/index">RICBL</a></li>
+          <li><a href="http://www.bhutaninsurance.com.bt/" target="_blank">Bhutan Insurance Ltd</a></li>
+          <li><a href="http://www.nppf.org.bt/" target="_blank">NPPF</a></li>
+          <li><a href="https://www.gicbhutanre.com/" target="_blank">GIC</a></li>
+        </ul>
+
+        <h6>Microfinance Institutions</h6>
+        <ul>
+          <li><a href="http://renewmicrofinance.com" target="_blank">RENEW MFI</a></li>
+          <li><a href="http://mbpl.bt" target="_blank">MBPL</a></li>
+          <li><a href="https://www.baowe.org/programmes/baowe-pelzing-microfinance-institution/ " target="_blank">BAOWE Pelzing</a></li>
+          <li><a href="https://tarayanamicrofinance.com/" target="_blank">Tarayana Microfinance</a></li>
+          <li><a href="https://carecredit.bt/" target="_blank">BCCL</a></li>
+        </ul>
+      </div>
+
+      <div className="links">
+        <h6>CSI Banks</h6>
+        <ul>
+          <li><a href="http://www.csibank.bt/" target="_blank">National CSI Development Bank Limited</a></li>
+        </ul>
+        <h6>Other Organizations</h6>
+        <ul>
+          <li><a href="http://www.mof.gov.bt/" target="_blank">Ministry of Finance</a></li>
+          <li><a href="http://www.bbs.bt/news/" target="_blank">Bhutan Broadcasting Service</a></li>
+          <li><a href="https://www.bt.bt/" target="_blank">Druknet</a></li>
+          <li><a href="http://www.kuenselonline.com/" target="_blank">Kuensel</a></li>
+          <li><a href="http://www.ecb.bt/" target="_blank">Election Commission of Bhutan</a></li>
+        </ul>
+      </div>
+
+      <div className="links">
+        <h6>Other Links</h6>
+        <ul>
+          <li><a href="https://www.newyorkfed.org/" target="_blank">FedReserve, N.Y</a></li>
+          <li><a href="https://www.rbi.org.in/" target="_blank">RBI, India</a></li>
+          <li><a href="https://www.adb.org/" target="_blank">Asian Development Bank</a></li>
+          <li><a href="http://www.worldbank.org/" target="_blank">World Bank</a></li>
+          <li><a href="http://www.imf.org/external/index.htm" target="_blank">International Monetary Fund</a></li>
+          <li><a href="https://www.money.co.uk/current-accounts/online-current-accounts.htm" target="_blank">Top 10 Online Banks</a></li>
+          <li><a href="https://www.bis.org/" target="_blank">Bank for International Settlements (BIS)</a></li>
+          <li><a href="https://www.asianclearingunion.org/" target="_blank">Asian Clearing Union (ACU)</a></li>
+          <li><a href="https://dbie.rbi.org.in/DBIE/dbie.rbi?site=saarcHome" target="_blank">SAARCFINANCE Database</a></li>
+        </ul>
+      </div>
+
+      <div className="links">
+        <h6>Board Of Directors of FIs</h6>
+        <ul>
+          <li><a href="https://www.rma.org.bt/board_of_director_bank.jsp" target="_blank">Banks</a></li>
+          <li><a href="https://www.rma.org.bt/board_of_director_insurance.jsp" target="_blank">Insurance</a></li>
+          <li><a href="https://www.rma.org.bt/board_of_director_pension.jsp" target="_blank">Pension</a></li>
+          <li><a href="https://www.rma.org.bt/board_of_director_micro_finance.jsp" target="_blank">Micro Finance Institution</a></li>
+          <li><a href="https://www.rma.org.bt/board_of_director_other.jsp" target="_blank">Other</a></li>
+        </ul>
+      </div>
     </div>
-  );
+ );
 }
+
