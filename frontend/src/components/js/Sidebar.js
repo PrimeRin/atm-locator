f501292b638fd08e02bf6faa88fa83481f6d9484
PrimeRin <primedruk312@gmail.com>
2024-05-09 15:26:55 +0600

    add profile and log out

--- frontend/src/components/js/Sidebar.js
+++ frontend/src/components/js/Sidebar.js
@@ -12,6 +12,11 @@ import {
 } from "react-icons/bs";
 import rma_logo from "../../assets/img/rma-logo.png";
 import usePath from "./usePath";
+import 
+ {BsFillCircleFill, BsJustify}
+ from 'react-icons/bs'
+ import profile from "../../assets/img/profile.jpeg";
+ import { CiLogout } from "react-icons/ci";
 
 function Sidebar({ openSidebarToggle, OpenSidebar, handleTabClick }) {
   const isActive = usePath();
@@ -35,22 +40,59 @@ function Sidebar({ openSidebarToggle, OpenSidebar, handleTabClick }) {
       </div>
 
       <ul className="sidebar-list">
-        <li className={`sidebar-list-item ${isActive('/admin-dashboard') ? 'active' : ''}`} onClick={() => handleTabClick('admin-dashboard')}>
+        <li
+          className={`sidebar-list-item ${
+            isActive("/admin-dashboard") ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("admin-dashboard")}
+        >
           <a href="">
             <BsGrid1X2Fill className="icon" /> Dashboard
           </a>
         </li>
-        <li className={`sidebar-list-item ${isActive('/admin-atm-list') ? 'active' : ''}`} onClick={() => handleTabClick('admin-atm-list')}>
+        <li
+          className={`sidebar-list-item ${
+            isActive("/admin-atm-list") ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("admin-atm-list")}
+        >
           <a href="">
             <BsFillArchiveFill className="icon" /> ATMS
           </a>
         </li>
-        <li className={`sidebar-list-item ${isActive('/admin-register-atm') ? 'active' : ''}`} onClick={() => handleTabClick('admin-register-atm')}>
+        <li
+          className={`sidebar-list-item ${
+            isActive("/admin-register-atm") ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("admin-register-atm")}
+        >
           <a href="">
             <BsFillGrid3X3GapFill className="icon" /> Register ATM
           </a>
         </li>
       </ul>
+
+      <div className="profile-and-log-out">
+
+        <div className="profile-header-right">
+          <div className="profile-profile-img-con">
+            <img src={profile} alt="Logo" className="profile-profile-img" />
+            <BsFillCircleFill className="profile-active-dot" size={20} />
+          </div>
+          <div className="profile-profile-details">
+            <span className="profile-profile-name">Bank Manager</span>
+            <span className="profile-bank-name">BOB</span>
+          </div>
+        </div>
+        
+        <div className="log-out-btn-con">
+        <CiLogout size={25}/>
+        <span className="log-out-btn">
+          Log out
+        </span>
+      </div>
+      </div>
+      
     </aside>
   );
 }
