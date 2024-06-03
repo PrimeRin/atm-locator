e1de4caf09a61899bb98d2604f06529267b815f5
PrimeRin <primedruk312@gmail.com>
2024-06-03 11:16:35 +0600

    resolve location issue

--- frontend/src/components/js/CurrentLocation.js
+++ frontend/src/components/js/CurrentLocation.js
@@ -1,12 +1,20 @@
 import { InfoWindow } from "@react-google-maps/api";
+import { useEffect } from "react";
 
 export default function CurrentLocation({ pos, onClick, onClose }) {
-    console.log(pos);
+  useEffect(() => {
+    if (!pos) {
+      alert("Please enable camera access to determine your current location.");
+    }
+  }, [pos]);
+
+  if (!pos) return null;
+
   return (
-    <div className="current-location" onClick={onClick}>
-      <InfoWindow position={pos} className='info-window' onCloseClick={onClose}>
+    <InfoWindow position={pos} onCloseClick={onClose}>
+      <div className="current-location" onClick={onClick}>
         <span>You are here!</span>
-      </InfoWindow>
-    </div>
+      </div>
+    </InfoWindow>
   );
 }
